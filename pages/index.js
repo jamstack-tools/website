d7344654c837e2575bbbc419180e53a9a840aee0
Irene Oppo <i.oppo@datocms.com>
2021-01-30 14:49:50 +0100

    SEO home

--- pages/index.js
+++ pages/index.js
@@ -62,27 +62,31 @@ function Homepage({ preview, page }) {
       </Space>
 
       <Space top={3}>
-        <InterstitialTitle subtitle="Whether you are the founder or just a fan, register it!">
+        <InterstitialTitle subtitle="Whether you are the founder or just a fan, register it to JAMstack tools!">
           Can't find your favourite tool for your <strong>JAMstack</strong>{' '}
           website?
         </InterstitialTitle>
-        <div className={styles.grid}>
-          <Result number="1" label={<>Who, me?</>}>
-            Did you just launch on Product Hunt? Do you have a solid{' '}
-            <strong>JAMstack startup</strong>? Or did you just discover a tool
-            that made developing your <strong>static site easier</strong>?{' '}
-            <strong>It's time to show it to the world!</strong>
-          </Result>
-          <Result number="2" label={<>You choose what to write!</>}>
-            You just need to enter some basic information to register your{' '}
-            <strong>JAMstack tool</strong>. Or go crazy with a long markdown
-            description. It's up to you!
-          </Result>
-          <Result number="3" label={<>See it online!</>}>
-            We usually take a few hours to check if everything is in order, but
-            after that, you will be able to see your tool in all of its glory in
-            our <strong>Browse section</strong>!
-          </Result>
+
+        <div className={styles.gridContainer}>
+          <h5 className={styles.gridTitle}>Promote your JAMstack tool</h5>
+          <div className={styles.grid}>
+            <Result number="1" label={<>Who, me?</>}>
+              Did you just launch on Product Hunt? Do you have a solid{' '}
+              <strong>JAMstack startup</strong>? Or did you just discover a tool
+              that made developing your <strong>static site easier</strong>?{' '}
+              <strong>It's time to show it to the world!</strong>
+            </Result>
+            <Result number="2" label={<>You choose what to write!</>}>
+              You just need to enter some basic information to register your{' '}
+              <strong>JAMstack tool</strong>. Or go crazy with a long markdown
+              description. It's up to you!
+            </Result>
+            <Result number="3" label={<>See it online!</>}>
+              We usually take a few hours to check if everything is in order,
+              but after that, you will be able to see your tool in all of its
+              glory in our <strong>Browse section</strong>!
+            </Result>
+          </div>
         </div>
       </Space>
     </Layout>
