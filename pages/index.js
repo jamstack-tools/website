2968d22a648506424d4d9058f3c8bb40b8acada7
Irene Oppo <i.oppo@datocms.com>
2021-02-04 17:06:49 +0100

    Pippone

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ import gql from 'graphql-tag';
 import Head from 'next/head';
 import { renderMetaTags } from 'react-datocms';
 import Space from 'components/Space';
+import SmartMarkdown from 'components/SmartMarkdown';
 
 import styles from './style.module.css';
 
@@ -19,6 +20,7 @@ export const getStaticProps = gqlStaticProps(
       page: homePage {
         seoKeywords
         schema
+        text(markdown: true)
         seo: _seoMetaTags {
           ...seoMetaTagsFields
         }
@@ -31,71 +33,81 @@ export const getStaticProps = gqlStaticProps(
 function Homepage({ preview, page }) {
   return (
     <Layout preview={preview}>
-      <Head>
-        {renderMetaTags(page.seo)}
-        <meta name="keywords" content={page.seoKeywords} />
-        {page.schema && (
-          <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{ __html: JSON.stringify(page.schema) }}
-          />
-        )}
-      </Head>
-      <Hero
-        title={'JAMstack tools'}
-        kicker={'The ultimate JAMstack showcase'}
-        subtitle={
-          <>
-            Build fast and secure modern websites. This is why we ♥ the
-            JAMstack.{' '}
-          </>
-        }
-      >
-        <Checks checks={["It's free!"]}>
-          <Button fs="big" as="a" href="/register">
-            Register your tool!
-          </Button>
-        </Checks>
-        <p>or</p>
-        <Checks checks={["It's also free!"]}>
-          <Button fs="big" as="a" href="/browse">
-            Browse
-          </Button>
-        </Checks>
-      </Hero>
+      <div className={styles.pageBg}>
+        <Head>
+          {renderMetaTags(page.seo)}
+          <meta name="keywords" content={page.seoKeywords} />
+          {page.schema && (
+            <script
+              type="application/ld+json"
+              dangerouslySetInnerHTML={{ __html: JSON.stringify(page.schema) }}
+            />
+          )}
+        </Head>
+        <Hero
+          title={'JAMstack tools'}
+          kicker={'The ultimate JAMstack showcase'}
+          subtitle={
+            <>
+              Build fast and secure modern websites. This is why we ♥ the
+              JAMstack.{' '}
+            </>
+          }
+        >
+          <Checks checks={["It's free!"]}>
+            <Button fs="big" as="a" href="/register">
+              Register your tool!
+            </Button>
+          </Checks>
+          <p>or</p>
+          <Checks checks={["It's also free!"]}>
+            <Button fs="big" as="a" href="/browse">
+              Browse
+            </Button>
+          </Checks>
+        </Hero>
 
-      <Space top={3}>
-        <PersonasPicker />
-      </Space>
+        <Space top={3}>
+          <PersonasPicker />
+        </Space>
 
-      <Space top={3}>
-        <InterstitialTitle subtitle="Whether you are the founder or just a fan, register it to JAMstack tools!">
-          Can't find your favourite tool for your <strong>JAMstack</strong>{' '}
-          website?
-        </InterstitialTitle>
+        <Space top={3}>
+          <InterstitialTitle subtitle="Whether you are the founder or just a fan, register it to JAMstack tools!">
+            Can't find your favourite tool for your <strong>JAMstack</strong>{' '}
+            website?
+          </InterstitialTitle>
 
-        <div className={styles.gridContainer}>
-          <h5 className={styles.gridTitle}>Promote your JAMstack tool</h5>
-          <div className={styles.grid}>
-            <Result number="1" label={<>Who, me?</>}>
-              Did you just launch on Product Hunt? Do you have a solid{' '}
-              <strong>JAMstack startup</strong>? Or did you just discover a tool
-              that made developing your <strong>static site easier</strong>?{' '}
-              <strong>It's time to show it to the world!</strong>
-            </Result>
-            <Result number="2" label={<>You choose what to write!</>}>
-              You just need to enter some basic information to register your{' '}
-              <strong>JAMstack tool</strong>. Or go crazy with a long markdown
-              description. It's up to you!
-            </Result>
-            <Result number="3" label={<>See it online!</>}>
-              We usually take a few hours to check if everything is in order,
-              but after that, you will be able to see your tool in all of its
-              glory in our <strong>Browse section</strong>!
-            </Result>
+          <div className={styles.gridContainer}>
+            <h5 className={styles.gridTitle}>Promote your JAMstack tool</h5>
+            <div className={styles.grid}>
+              <Result number="1" label={<>Who, me?</>}>
+                Did you just launch on Product Hunt? Do you have a solid{' '}
+                <strong>JAMstack startup</strong>? Or did you just discover a
+                tool that made developing your{' '}
+                <strong>static site easier</strong>?{' '}
+                <strong>It's time to show it to the world!</strong>
+              </Result>
+              <Result number="2" label={<>You choose what to write!</>}>
+                You just need to enter some basic information to register your{' '}
+                <strong>JAMstack tool</strong>. Or go crazy with a long markdown
+                description. It's up to you!
+              </Result>
+              <Result number="3" label={<>See it online!</>}>
+                We usually take a few hours to check if everything is in order,
+                but after that, you will be able to see your tool in all of its
+                glory in our <strong>Browse section</strong>!
+              </Result>
+            </div>
           </div>
-        </div>
-      </Space>
+        </Space>
+
+        <Space top={3}>
+          <InterstitialTitle>What is JAMstack?</InterstitialTitle>
+          <div className={styles.mdText}>
+            <SmartMarkdown>{page.text || ''}</SmartMarkdown>
+          </div>
+        </Space>
+      </div>
     </Layout>
   );
 }
